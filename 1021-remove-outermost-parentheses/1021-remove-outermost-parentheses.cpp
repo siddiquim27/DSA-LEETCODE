23f29450d8fd3dbe42a12f84f9557a638a3ca1de
Numan Siddiqui <numansiddiqui7863@gmail.com>
2026-02-06 22:24:58 +0530

    Time: 0 ms (100%), Space: 8.9 MB (80.58%) - LeetHub

--- 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.cpp
+++ 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.cpp
@@ -3,16 +3,14 @@ public:
     string removeOuterParentheses(string s) {
         int cnt = 0;
         int n = s.length();
-        string ans ="";
+        string ans = "";
+
+
         for(int i = 0; i < n; i++){
             if(s[i] == ')') cnt--;
             if(cnt != 0) ans.push_back(s[i]);
             if(s[i] == '(') cnt++;
-           
-                
-                
-            } 
-        
+        }
         return ans;
     }
 };
